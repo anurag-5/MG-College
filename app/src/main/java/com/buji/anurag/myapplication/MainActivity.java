b5da1dcd2de442d3f10ea7c9976688d2008d29dd
anurag-5 <anuragrajeshayyappanmala45@gmail.com>
2019-12-06 11:13:48 +0530

    login

--- app/src/main/java/com/buji/anurag/myapplication/MainActivity.java
+++ app/src/main/java/com/buji/anurag/myapplication/MainActivity.java
@@ -2,12 +2,27 @@ package com.buji.anurag.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et,et2;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et=(EditText)findViewById(R.id.ed);
+        et2=(EditText)findViewById((R.id.ed2));
+        btn=(Button)findViewById((R.id.bt));
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
